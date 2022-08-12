b30fe4ee1f5062ceabbc3a5b049ea0ccbb835990
Jakub Laczniak <kuba.laczniak1@gmail.com>
2022-08-12 15:31:50 +0200

    more styling

--- src/App.js
+++ src/App.js
@@ -25,11 +25,36 @@ function App() {
           <section className='videoBox__description'>
             <h1 className='description__title'>{videoTitle}</h1>
             <p className='description__text'>{videoDescription}</p>
-            <div className='description__icons'></div>
+            <div className='description__icons'>
+              <div className='icons__wrapper'>
+                <div className='icons__iconWrapper view'/>
+                <p>12,434,934</p>
+              </div>
+              <div className='icons__wrapper'>
+                <div className='icons__iconWrapper heart'/>
+                <p>6,563</p>
+              </div>
+              <div className='icons__wrapper'>
+                <div className='icons__iconWrapper comment'/>
+                <p>831</p>
+              </div>
+            </div>
           </section>
           <section className='videoBox__footer'>
-            <div className='footer__socialMedia'></div>
-            <div className='footer__date'></div>
+            <div className='footer__socialMedia'>
+              <div className='socialMedia__portrait'/>
+              <div className='socialMedia__userData'>
+                <p className='userData__name'>Sara Jameson</p>
+                <p className='userData__followers'>12,353 followers</p>
+              </div>
+              <div className='socialMedia__button'>
+                <div className='button__cross'/><p>Follow</p>
+              </div>
+            </div>
+            <div className='footer__date'>
+                <p className='date__head'>Uploaded:</p>
+                <p className='date'>8 March, 2007</p>
+            </div>
           </section>
         </article>
 
--- src/components/SidebarVideo.js
+++ src/components/SidebarVideo.js
@@ -3,8 +3,15 @@ import React from 'react'
 function SidebarVideo({ data }) {
     const { title, duration, views } = data;
   return (
-    <div>
-        {title}
+    <div className='content__filmBox'>
+      <div className='filmBox__film'>
+        <span>{duration}</span>
+      </div>
+      <h3>{title}</h3>
+      <div className='filmBox__footer'>
+        <div className='filmBox__iconWrapper'/>
+        <p>{views}</p>
+      </div>
     </div>
   )
 }
